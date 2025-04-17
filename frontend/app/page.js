73ac1160387a597077d7e7f83d50689987dfabb0
Subhraneel Saha <subhraneelsaha9674587481@gmail.com>
2025-04-17 21:23:15 +0530

    commits

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -8,7 +8,7 @@ export default function HomePage() {
 
   return (
     <div className="p-6">
-      <h1 className="text-3xl font-bold mb-4">🏫 Smart Campus Dashboard</h1>
+      <h1 className="text-3xl font-bold mb-4">🏫Dashboard</h1>
 
       {account ? (
         <div>
--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -3,17 +3,17 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="bg-blue-700 text-white p-4 flex justify-between">
+    <nav className="bg-red-700 text-white p-4 flex justify-between">
       <h1 className="font-bold text-lg">🎓 Smart Campus Toolkit</h1>
       <div className="space-x-4">
-        <Link href="/">Home</Link>
-        <Link href="/attendance">Attendance</Link>
-        <Link href="/booking">Booking</Link>
-        <Link href="/certificates">Certificates</Link>
-        <Link href="/fees">Fees</Link>
-        <Link href="/lostfound">Lost & Found</Link>
-        <Link href="/feedback">Feedback</Link>
-        <Link href="/admin">Admin</Link>
+        <Link href="/">Home   </Link>
+        <Link href="/attendance">Attendance   </Link>
+        <Link href="/booking">Booking   </Link>
+        <Link href="/certificates">Certificates   </Link>
+        <Link href="/fees">Fees   </Link>
+        <Link href="/lostfound">Lost & Found    </Link>
+        <Link href="/feedback">Feedback   </Link>
+        <Link href="/admin">Admin   </Link>
       </div>
     </nav>
   );
